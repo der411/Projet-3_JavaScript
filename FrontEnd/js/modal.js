e1c6cf5c665b165478795d3ff44dd0f54a692650
der411 <vaitilingomvincent@gmail.com>
2024-04-30 13:25:17 +0200

    revue du code js modale

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -17,10 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Fermeture de la modale demandée");
     if (modal === null) return;
     e.preventDefault();
-    e.stopPropagation();
     modal.style.display = "none";
-    modal.setAttribute("aria-hidden", "true");
-    modal.removeAttribute("aria-modal");
     modal.removeEventListener("click", closeModal);
     modal.querySelector(".js-modal-close").removeEventListener("click", closeModal);
     modal.querySelector(".js-modal-stop").removeEventListener("click", stopPropagation);
@@ -32,8 +29,6 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     const target = document.querySelector(e.target.getAttribute("href"));
     target.style.display = null;
-    target.removeAttribute("aria-hidden");
-    target.setAttribute("aria-modal", "true");
     modal = target;
     modal.addEventListener("click", closeModal);
     modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
@@ -49,6 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("gallery-view").style.display = "none";
     document.getElementById("add-photo-view").style.display = "block";
     document.getElementById("button-back").style.visibility = "visible";
+    
     document.getElementById("submit-photo").addEventListener("click", (e) => {
       e.preventDefault();
       addWork(e);
@@ -58,8 +54,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // Fonction pour retourner à la vue de la galerie
   const showGalleryView = (e) => {
     e.preventDefault();
-    e.stopPropagation();
-    console.trace("Trace pour deleteWork");
     document.getElementById("add-photo-view").style.display = "none";
     document.getElementById("gallery-view").style.display = "block";
     document.getElementById("button-back").style.visibility = "hidden";
@@ -79,7 +73,7 @@ const getCategories = async () => {
     const response = await fetch("http://localhost:5678/api/categories", {
       method: "GET",
       headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"), // Ajoutez votre token d'autorisation
+        Authorization: "Bearer " + localStorage.getItem("token"), // Token d'autorisation
       },
     });
 
@@ -104,28 +98,24 @@ const getCategories = async () => {
       });
     } else {
       console.error("Erreur lors de la récupération des catégories", categories);
-      // Logique pour gérer l'échec (par exemple, montrer un message à l'utilisateur)
     }
   } catch (error) {
     console.error("Erreur lors de la connexion à l'API", error);
-    // Logique pour gérer les erreurs réseau ou de connexion
   }
 };
-
-// Appeler la fonction au chargement de la page
+// Appel la fonction au chargement de la page
 window.onload = getCategories;
 
-// Fonction asynchrone pour ajouter un travail
-
+// Fonction asynchrone pour ajouter un work
 const addWork = async (event) => {
   event.preventDefault();
 
-  const title = document.getElementById("photo-title").value.trim();
+  const title = document.getElementById("photo-title").value;
   const fileInput = document.getElementById("file-upload");
   const imageFile = fileInput.files[0];
   const categoryId = document.getElementById("photo-category").value;
 
-  // S'assurer que tous les champs sont remplis
+  // Vérifie que tous les champs sont remplis
   if (!title || !imageFile || categoryId === undefined) {
     alert("Merci de compléter tous les champs");
     return;
@@ -134,7 +124,7 @@ const addWork = async (event) => {
   let formData = new FormData();
   formData.append("title", title);
   formData.append("image", imageFile);
-  formData.append("category", categoryId); // Utiliser la chaîne de caractères directement
+  formData.append("category", categoryId);
 
   document.getElementById("photo-title").value = "";
   document.getElementById("file-upload").value = "";
@@ -145,12 +135,12 @@ const addWork = async (event) => {
     const response = await fetch("http://localhost:5678/api/works", {
       method: "POST",
       headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"), // Ajoutez votre token d'autorisation
+        Authorization: "Bearer " + localStorage.getItem("token"), // Token d'autorisation
       },
       body: formData,
     });
 
-    const result = await response.json(); // Parsez la réponse en JSON
+    const result = await response.json(); // Parse la réponse en JSON
     if (response.ok) {
       console.log("Work ajouté avec succès", result);
 
@@ -174,18 +164,18 @@ const addWork = async (event) => {
 document.getElementById("file-upload").addEventListener("change", function () {
   const file = this.files[0];
 
-  // Nettoyer le conteneur d'aperçu précédent
+  // Nettoie le conteneur d'aperçu précédent
   imagePreview.innerHTML = "";
 
   if (file && file.type.startsWith("image/")) {
     const reader = new FileReader();
     reader.onload = function (e) {
-      // Masquer les éléments non nécessaires
+      // Masque les éléments non nécessaires
       uploadIcon.style.display = "none";
       addButton.style.display = "none";
       fileTypeText.style.display = "none";
 
-      // Créer et afficher l'aperçu de l'image
+      // Crée et afficher l'aperçu de l'image
       const image = new Image();
       image.src = e.target.result;
       image.id = "image-preview";
@@ -207,10 +197,8 @@ document.getElementById("file-upload").addEventListener("change", function () {
 });
 
 //Change la couleur du bouton Valider lorsqu'il est cliqué
-document.addEventListener("DOMContentLoaded", function () {
   const submitButton = document.getElementById("submit-photo");
-
   submitButton.addEventListener("click", function () {
     submitButton.style.backgroundColor = "#1d6154";
   });
-});
+
